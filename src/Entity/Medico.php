eaadafae46ec50e95b3b0ff4142234564fde6499
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-03-25 18:06:19 -0300

    initializing HATEOAS

--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -77,7 +77,15 @@ class Medico implements \JsonSerializable
             'id' => $this->getId(),
             'crm' => $this->getCrm(),
             'nome' => $this->getNome(),
-            'especialidadeId' => $this->getEspecialidade()->getId()
+            'especialidadeId' => $this->getEspecialidade()->getId(),
+            '_links' => [
+                'rel' => 'self',
+                'path' => '/medicos/' . $this->getId()
+            ]
+            ,[
+                'rel' => 'especialidade',
+                'path' => '/especialidades/' . $this->getEspecialidade()->getId()
+            ]
         ];
     }
 }
